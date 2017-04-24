93d1abd3c0e23b4f44608f1615dab3982bad926e
sarindy <sarindyo@yahoo.com>
2017-04-24 08:11:55 +0700

    Contact Service Change

--- src/main/java/com/sarindy/contact/services/ContactService.java
+++ src/main/java/com/sarindy/contact/services/ContactService.java
@@ -3,8 +3,13 @@ package com.sarindy.contact.services;
 import com.sarindy.responseCode.ResponseCodeModel;
 
 public interface ContactService {
+
+	public ResponseCodeModel addContact(String title, String firstName, String lastName, String emailAddress, String phoneNumber, String address,
+			int lastModifiedBy);
 	
-	public ResponseCodeModel addContact(String title,String firstName, String lastName, String emailAddress,String phoneNumber,String address,int lastModifiedBy);
+	public ResponseCodeModel updateContact(String title, String firstName, String lastName, String emailAddress, String phoneNumber, String address,
+			int lastModifiedBy);
+	
 	
 
 }
